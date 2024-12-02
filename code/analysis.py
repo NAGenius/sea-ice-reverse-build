275c22ab8c26a3b947bb0a64eb08c553ac70150a
NAGenius <2372575297@qq.com>
2024-12-03 01:39:17 +0800

    得到基本的excel文件

--- code/analysis.py
+++ code/analysis.py
@@ -12,12 +12,10 @@ from utils.get_data import read_tif, coord2xy
 def get_concentration(year, _lat, _lon):
     _data = read_tif(f'../concentration/data/N_{year}_concentration_v3.0.tif')[0]
     # x_coords, y_coords = coord2xy((447, 303), geotransform)  # 第447行303列
-    # print(x_coords, y_coords)
     for j in range(len(_lat)):
         x, y = transformer2.transform(_lat[j], _lon[j])
-        # print(transformer.transform(x, y), _lat[j], _lon[j])
-        col, row = np.searchsorted(x_coords, x, side='left') - 1, np.searchsorted(-y_coords, y, side='left') - 1
-        if _data[row][col] in [2530, 2540]:
+        col, row = int((x - x_coords[0]) / 25000), int((y_coords[0] - y) / 25000)
+        if _data[row][col] in [0, 2530, 2540, 2550]:
             for k in range(len(dx)):
                 if 0 < _data[row + dy[k]][col + dx[k]] < 1000:
                     _data[row][col] = _data[row + dy[k]][col + dx[k]]
@@ -32,18 +30,28 @@ def get_thickness(year, _lat, _lon):
         x, y = to3411(_lon, _lat)
         for j in range(len(x)):
             col, row = int((x[j] - x_coords2[0][0]) / 25000), int((y_coords2[0][0] - y[j]) / 25000)
+            if _data[row][col] == -9999:
+                for k in range(len(dx)):
+                    if _data[row + dy[k]][col + dx[k]] != -9999:
+                        _data[row][col] = _data[row + dy[k]][col + dx[k]]
+                        break
             thickness.append(_data[row][col])
     else:
         _data = np.array(_nc_obj.variables['thickness'][:])
         x, y = to3408(_lon, _lat)
         for j in range(len(x)):
             col, row = int((x[j] - x_coords3[0][0]) / 100000), int((y[j] - y_coords3[0][0]) / 100000)
+            if _data[row][col] == np.nan:
+                for k in range(len(dx)):
+                    if _data[row + dy[k]][col + dx[k]] != np.nan:
+                        _data[row][col] = _data[row + dy[k]][col + dx[k]]
+                        break
             thickness.append(_data[row][col])
 
 
 if __name__ == '__main__':
-    dx = [0, 0, 1, -1]
-    dy = [1, -1, 0, 0]
+    dx = [0, 0, 1, -1, -1, -1, 1, 1]
+    dy = [1, -1, 0, 0, -1, 1, -1, 1]
     data, geotransform, transformer = read_tif('../concentration/data/N_19800909_concentration_v3.0.tif')
     transformer2 = Transformer.from_crs(transformer.target_crs, transformer.source_crs)
     x_coords, _ = coord2xy((0, np.arange(304)), geotransform)
@@ -56,7 +64,7 @@ if __name__ == '__main__':
     to3411 = Proj('epsg:3411')
     x_coords2, y_coords2 = to3411(lon, lat)
     x_coords2 -= 25000 / 2
-    y_coords2 -= 25000 / 2
+    y_coords2 += 25000 / 2
 
     nc_obj = Dataset('../motion/data/icemotion_daily_nh_25km_19790101_19791231_v4.1.nc', 'r')
     crs_wkt = nc_obj.variables['crs'].crs_wkt
@@ -67,6 +75,14 @@ if __name__ == '__main__':
     nc_obj.close()
     to3408 = Proj(crs_wkt)
     x_coords3, y_coords3 = to3408(lon, lat)
+    concentrations, thickness = [], []
+
+    # print(x_coords[0], y_coords[0], x_coords[-1], y_coords[-1], len(y_coords))
+    # print(x_coords2[0][0], y_coords2[0][0], x_coords2[0][-1], y_coords2[-1][0], y_coords2.shape)
+    # get_concentration(20160913, [88.42886], [-110.83542])
+    # get_thickness(2016, [88.42886], [-110.83542])
+    # print(concentrations)
+    # print(thickness)
 
     nc_obj = Dataset('../result/start/nc/all.nc', 'r')
     lat = np.array(nc_obj.variables['lat'][:])
@@ -85,13 +101,13 @@ if __name__ == '__main__':
         "1986": "19850905",
     }
     tmp_lat, tmp_lon = [], []
-    concentrations, thickness = [], []
     for i in range(len(lat)):
         if str(int(dates[i][:4]) + 1) in missing:
             dates[i] = missing[str(int(dates[i][:4]) + 1)]
-        if dates[i] != dates[i - 1] and i:
-            get_concentration(dates[i], tmp_lat, tmp_lon)
-            get_thickness(dates[i][:4], tmp_lat, tmp_lon)
+        if dates[i] != dates[i - 1] and i != 0:
+            print(dates[i])
+            get_concentration(dates[i - 1], tmp_lat, tmp_lon)
+            get_thickness(dates[i - 1][:4], tmp_lat, tmp_lon)
             tmp_lat, tmp_lon = [], []
         tmp_lat.append(lat[i])
         tmp_lon.append(lon[i])
@@ -100,6 +116,16 @@ if __name__ == '__main__':
         get_concentration(dates[-1], tmp_lat, tmp_lon)
         get_thickness(dates[-1][:4], tmp_lat, tmp_lon)
 
+    cnt = 0
+    for i in range(len(thickness)):
+        if thickness[i] == -9999:
+            cnt += 1
+    print(cnt, len(thickness))
+    cnt = 0
+    for i in range(len(concentrations)):
+        if concentrations[i] in [0, 2530, 2540, 2550]:
+            cnt += 1
+    print(cnt, len(concentrations))
     data = {
         '纬度(latitude)': lat,
         '经度(longitude)': lon,
